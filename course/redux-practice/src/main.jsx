8ccf848c363720c6b0fbf834e93f5b14366fa86f
YC-TAN <150326068+YC-TAN@users.noreply.github.com.>
2026-03-03 12:41:17 +1300

    Counter initial set up

--- course/redux-practice/src/main.jsx
+++ course/redux-practice/src/main.jsx
@@ -1,10 +1,57 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createStore } from "redux";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const counterReducer = (state = 0, action) => {
+  switch (action.type) {
+    case "INCREMENT":
+      return state + 1;
+    case "DECREMENT":
+      return state - 1;
+    case "ZERO":
+      return 0;
+    default:
+      return state;
+  }
+};
+
+const store = createStore(counterReducer);
+
+store.subscribe(() => {
+  console.log(store.getState());
+});
+
+store.dispatch({ type: "INCREMENT" });
+store.dispatch({ type: "INCREMENT" });
+store.dispatch({ type: "DECREMENT" });
+store.dispatch({ type: "INCREMENT" });
+store.dispatch({ type: "ZERO" });
+store.dispatch({ type: "INCREMENT" });
+
+const App = () => {
+  return (
+    <div>
+      <div>{store.getState()}</div>
+      <button onClick={() => store.dispatch({ type: "INCREMENT" })}>
+        plus
+      </button>
+      <button onClick={() => store.dispatch({ type: "DECREMENT" })}>
+        minus
+      </button>
+      <button onClick={() => store.dispatch({ type: "ZERO" })}>zero</button>
+    </div>
+  );
+};
+
+const root = createRoot(document.getElementById("root"));
+
+const renderApp = () => {
+  root.render(
+    <StrictMode>
+      <App />
+    </StrictMode>,
+  );
+};
+
+renderApp();
+store.subscribe(renderApp);
