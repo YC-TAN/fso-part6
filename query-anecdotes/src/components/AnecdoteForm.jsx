2852ad4b559eb28fdd6883f21eecaf2dc24c6480
YC-TAN <150326068+YC-TAN@users.noreply.github.com.>
2026-03-05 13:59:57 +1300

    Implement notification using context API

--- query-anecdotes/src/components/AnecdoteForm.jsx
+++ query-anecdotes/src/components/AnecdoteForm.jsx
@@ -1,22 +1,49 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query'
 import { createAnecdote } from '../requests'
+import { useContext } from 'react'
+import NotificationContext from '../NotificationContext'
 
 const AnecdoteForm = () => {
   const queryClient = useQueryClient()
+  const { notificationDispatch } = useContext(NotificationContext)
 
   const newAnecdoteMutation = useMutation({
     mutationFn: createAnecdote,
     onSuccess: (newAnecdote) => {
       const anecdotes = queryClient.getQueryData(['anecdotes'])
       queryClient.setQueryData(['anecdotes'], anecdotes.concat(newAnecdote))
-    }
+      notificationDispatch({
+        type: 'NOTIFY',
+        payload: {
+          message: `anecdote ${newAnecdote.content} created`,
+          type: 'success',
+        },
+      })
+      setTimeout(() => {
+        notificationDispatch({ type: 'CLEAR' })
+      }, 5000)
+    },
+    onError: (error) => {
+        // 'error' is the object returned by your request (e.g., from axios)
+        const errorMessage = error.error || 'Something went wrong'
+        
+        notificationDispatch({
+          type: 'NOTIFY',
+          payload: { message: errorMessage, type: 'error' }
+        })
+
+        // Clear error message after 5 seconds
+        setTimeout(() => {
+          notificationDispatch({ type: 'CLEAR' })
+        }, 5000)
+      }
   })
 
   const onCreate = (event) => {
     event.preventDefault()
     const content = event.target.anecdote.value
     event.target.anecdote.value = ''
-    newAnecdoteMutation.mutate({ content, votes: 0})
+    newAnecdoteMutation.mutate({ content, votes: 0 })
   }
 
   return (
